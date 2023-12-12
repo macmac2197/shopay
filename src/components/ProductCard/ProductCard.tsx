854f31e68ddfdf4bc8c899a1d120ceee90079e96
Mark Arjel Cabais <mapcabais@unionbankph.com>
2023-12-12 15:47:22 +0800

    add hover events for product card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,38 +1,87 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { ProductInterface } from "../../types/ProductInterface";
 import { ReactComponent as HeartIcon } from "../../assets/icons/heart.svg";
 import { ReactComponent as GitCompare } from "../../assets/icons/git-compare.svg";
+import { ReactComponent as Star } from "../../assets/icons/star.svg";
 import "./ProductCard.css";
 
-const ProductCard = () => {
+const ProductCard: React.FC<ProductInterface> = (product: ProductInterface) => {
+  const {
+    id,
+    title,
+    price,
+    discountPercentage,
+    rating,
+    stock,
+    category,
+    thumbnail,
+  } = product;
+
+  const [isShowAdd, setIsShowAdd] = useState<boolean>(false);
+  const [isProductActive, setIsProductActive] = useState<boolean>(false);
+  const [isShowOutOfStock, setIsShowOutOfStock] = useState<boolean>(false);
+
+  const handleMouseOver = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+
+    // check product stock
+    if (stock > 0) {
+      setIsShowAdd(true); // show add to basket button
+      setIsShowOutOfStock(false); // hide out of stock label
+    } else {
+      setIsShowOutOfStock(true); // show out of stock label
+    }
+    setIsProductActive(true); // set product active when user hovers over product
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+
+    setIsShowAdd(false); // hide add to basket button
+    setIsProductActive(false); // set product in-active when user hoversout over product
+    setIsShowOutOfStock(false); // hide out of stock label
+  };
+
   return (
-    <div className="card">
+    <div
+      className="card"
+      key={id}
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="card-content">
         <div className="card-img">
-          <img
-            src="https://i.dummyjson.com/data/products/5/thumbnail.jpg"
-            alt="Product"
-          />
+          <img src={thumbnail} alt={title} />
         </div>
-        <div className="card-stock">Out of stock</div>
-        <div className="card-actions">
-          <div className="actions-btn">
-            <GitCompare />
-          </div>
-          <div className="actions-btn">
-            <HeartIcon />
-          </div>
-        </div>
-        <div className="card-discount">35%</div>
+        {isShowOutOfStock && <div className="card-stock">Out of stock</div>}
+        {isProductActive && (
+          <React.Fragment>
+            <div className="card-actions">
+              <div className="actions-btn">
+                <GitCompare />
+              </div>
+              <div className="actions-btn">
+                <HeartIcon />
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+        <div
+          className={`card-discount ${isProductActive && "text-red"}`}
+        >{`${discountPercentage}%`}</div>
       </div>
-      <div className="card-category">Category</div>
-      <div className="card-title">Card Title</div>
-      <div className="card-price">Card Price</div>
-      <div className="card-rating">Card Rating</div>
-      <div className="card-divider" />
-      <div className="card-button">
-        <button type="button">Add To Basket</button>
-      </div>
+      <div className="card-category">{category}</div>
+      <div className="card-title">{title}</div>
+      <div className="card-price">{`€ ${price}`}</div>
+      <div className="card-rating">{rating}</div>
+      {isShowAdd && (
+        <React.Fragment>
+          <div className="card-divider" />
+          <div className="card-button">
+            <button type="button">Add To Basket</button>
+          </div>
+        </React.Fragment>
+      )}
     </div>
   );
 };
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import ProductCard from "../ProductCard/ProductCard";
+import { ProductInterface } from "../../types/ProductInterface";
+
+const mockProduct: ProductInterface = {
+  id: 1,
+  title: "iPhone 9",
+  description: "An apple mobile which is nothing like apple",
+  price: 549,
+  discountPercentage: 12.96,
+  rating: 4.69,
+  stock: 94,
+  brand: "Apple",
+  category: "smartphones",
+  thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+  images: [
+    "https://i.dummyjson.com/data/products/1/1.jpg",
+    "https://i.dummyjson.com/data/products/1/2.jpg",
+    "https://i.dummyjson.com/data/products/1/3.jpg",
+    "https://i.dummyjson.com/data/products/1/4.jpg",
+    "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+  ],
+};
 
 const Products = () => {
   return (
     <div className="products">
-      <ProductCard />
+      <ProductCard {...mockProduct} />
     </div>
   );
 };
